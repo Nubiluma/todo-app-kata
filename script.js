98e40cae60602d697d08660c72ffe92b33e7909e
Sandra Petereit <sandrap3393@googlemail.com>
2022-11-11 19:59:01 +0100

    add functionality: new entry as checkbox

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const addBtn = document.querySelector("#btn-add");
 const input = document.querySelector("#todo-input");
-const toDoList = document.querySelector("#todo-list");
+const toDoList = document.querySelector("#todos");
 
-const toDos = [];
+const filterOptions = ["all", "done", "open"];
+
+const appState = {
+  filter: "all",
+  todos: [],
+};
 
 class toDoItem {
-  constructor(value, progress) {
-    this.value = value;
-    this.progress = progress;
+  constructor(description, isDone) {
+    this.description = description;
+    this.isDone = isDone;
   }
 }
 
@@ -16,26 +21,54 @@ addBtn.addEventListener("click", function () {
 });
 
 /**
- * add input value to list container
+ * add input value to list
  * @param {*} event
  */
 function addTodoItem(event) {
-  const newEntry = new toDoItem(input.value, "open");
-  toDos.push(newEntry);
+  const newEntry = new toDoItem(input.value, false);
+  appState.todos.push(newEntry);
+  //toDos.push();
 
-  let li = document.createElement("li");
-  li.append(newEntry.value);
-  toDoList.appendChild(li);
+  let entryDiv = document.createElement("div");
+
+  let todoEntry = document.createElement("input");
+  todoEntry.setAttribute("type", "checkbox");
+  todoEntry.setAttribute("id", generateId());
+
+  console.log(generateId);
+
+  let entryLabel = document.createElement("label");
+  entryLabel.setAttribute("for", todoEntry.id);
+  entryLabel.append(newEntry.description);
+
+  toDoList.appendChild(entryDiv);
+  entryDiv.appendChild(todoEntry);
+  entryDiv.appendChild(entryLabel);
 
   //debug
   console.log("Added Entry: " + input.value);
-  console.log(toDos);
+  console.log(appState);
 }
 
-function toggleProgress() {}
+function removeTodoItem(event) {}
 
-function removeTodoItem() {}
+function toggleProgress(event) {
+  let entry = event.target;
+  entry.classList.toggle("todo--status");
+}
 
-function filterTodoItems() {}
+/*
+
+function filterTodoItems(event) {}
 
 function render() {}
+*/
+
+/**
+ * generate id for specific label and input relation
+ */
+let idCounter = 0;
+function generateId() {
+  idCounter++;
+  return idCounter.toString();
+}
